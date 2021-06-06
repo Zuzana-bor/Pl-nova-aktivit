a3e8550947076630ff887d3b90afb5fed88d0b56
Zuzana Borutova <z.kasikova@gmail.com>
2021-06-06 16:08:08 +0200

    post-obrazek

--- src/pages/Topic/Topic.jsx
+++ src/pages/Topic/Topic.jsx
@@ -16,8 +16,9 @@ export const Topic = () => {
       <h1 className="title">{topic}</h1>
       {relatetPosts.map((post) => (
         <article className="onePost">
+          <img src={post.image} alt={post.alt} />
           <h2>{post.title}</h2>
-          <section>{post.content.slice(0,150)}...</section>
+          <section>{post.content.slice(0, 150)}...</section>
         </article>
       ))}
     </>
