d9ff3a55baa0d70b65960f6f8ed87e1163c53313
Zuzana Borutova <z.kasikova@gmail.com>
2021-06-11 16:21:22 +0200

    oprava chyb- search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,19 +2,17 @@ import React, { useState } from 'react';
 import './search.css';
 import { posts } from '../../data';
 
-export const Search = ({searching}) => {
-  
-const handleSubmit = (result) => {
-  searching(result);
-   
+export const Search = ({ searching }) => {
+  const handleSubmit = (result) => {
+    searching(result);
   };
-  
+
   return (
     <>
       <form onSubmit={handleSubmit} id="formOfSearch" className="search">
         <input
           onChange={(event) => setListOfTopic(event.target.value)}
-          value={listOfTopic}
+          // value={listOfTopic}
           id="query"
           type="text"
           placeholder="hledej"
